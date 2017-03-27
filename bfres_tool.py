7d8280e0407bd8d3bf21f1ee324b1088c330cb4b
AboodXD <aboood40091@gmail.com>
2017-03-27 20:08:10 +0400

    Update bfres_tool.py

--- bfres_tool.py
+++ bfres_tool.py
@@ -1,3 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# BFRES Tool
+# Copyright © 2017 AboodXD
+
+# This file is part of BFRES Tool.
+
+# BFRES Tool is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# BFRES Tool is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""bfres_tool.py: The main executable."""
+
 import os, sys, struct, time
 
 def err():
